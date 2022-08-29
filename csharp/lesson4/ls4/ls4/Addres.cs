dfb515176a4fc58bcd53faa78526c857e166dc72
Nurik <nurkworld534@gmail.com>
2022-08-29 21:53:01 +0600

    Task1

---
+++ csharp/lesson4/ls4/ls4/Addres.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ls4
+{
+    internal class Address
+    {
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string House { get; set; }
+        public Address(string Country, string Cite, string Street, string House)
+        {
+            this.Country = Country;
+            this.City = City;
+            this.Street = Street;
+            this.House = House;
+        }
+    }
+}
---
+++ csharp/lesson4/ls4/ls4/UserService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ls4
+{
+    class UserService
+    {
+        public UserService()
+        {
+            Users = mew User[2];
+            Users[0] = new User()
+            {
+                Name = "Nurzhan", SecondName = "Seisenbay", Birth = new DateTime(2004, 07, 15)
+            };
+        }
+            
+        public User[] Users { get; set; }
+    }
+}
