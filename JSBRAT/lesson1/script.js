88f98275421a247dd02561b90feb19e2ce7d998a
Nurik <nurkworld534@gmail.com>
2022-09-02 20:04:26 +0600

    taskaddArray

--- JSBRAT/lesson1/script.js
+++ JSBRAT/lesson1/script.js
@@ -16,7 +16,7 @@ function printArray(array){
 
 function onlyEven(array){
 const result = [];
-for(let i; i < array.lenth; i++){
+for(let i; i < array.length; i++){
     if(array[i] % 2 === 0){
         result.push(array);
     }
@@ -25,5 +25,28 @@ for(let i; i < array.lenth; i++){
 printArray(result);
 }
 
+function arraySum(array){
+    let sum = 0;
+    for(let i = 0; i < array.length; i++){
+        sum += array[i];
+    }
+    console.log(sum);
+}
 
-onlyEven(arrayInt);
+function arrayMax(array){
+    const result = [...array];
+    result.sort(function(a, b){
+        return a - b;
+    });
+    console.log(result.pop());
+}
+
+function addArray(array, index , item){
+    const result = [...array];
+    result.splice(index, 0, item);
+    console.log(result);
+}
+
+arrayMax(arrayInt);
+printArray(arrayInt);
+addArray(arrayInt, 5, 'Back');
