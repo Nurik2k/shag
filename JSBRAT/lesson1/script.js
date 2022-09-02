a2539c490cc6fefc1558f4b1c88143dacf47135a
Nurik <nurkworld534@gmail.com>
2022-09-02 21:21:10 +0600

    FruitTask

--- JSBRAT/lesson1/script.js
+++ JSBRAT/lesson1/script.js
@@ -4,10 +4,13 @@ function getRandomNum(){
 }
 
 const arrayInt = [];
-
+const array2 = [];
 
 for(let i = 0; i < 10; i++){
     arrayInt.push(getRandomNum());
+}
+for(let i = 0; i < 5; i++){
+    array2.push(getRandomNum());
 }
 
 function printArray(array){
@@ -47,6 +50,43 @@ function addArray(array, index , item){
     console.log(result);
 }
 
-arrayMax(arrayInt);
-printArray(arrayInt);
-addArray(arrayInt, 5, 'Back');
+function deleteItem(pos, array){
+    const result = [...array];
+    result.splice(pos, 1);
+    console.log(result);
+}
+
+const arrayOne = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const arrayTwo = [1, 2, 3, 4, 5];
+
+function concatUniq(array1, array2){
+    //const result = [...new Set([...array1, ...array2])];
+const result = array1.concat(array2);
+const uniq = [...new Set(result)];
+console.log(result);
+}
+
+function concatDouble(array1, array2){
+    const result = array1.filter(function(item){
+        return array2.indexOf(item) > -1;
+    })
+    console.log(result);
+}
+function Uniq(array1, array2){
+    const result = array1.filter(function(item){
+        return array2.indexOf(item) === -1
+    })
+    console.log(result);
+}
+
+const fruits = ['ананас', 'яблоко', 'персик', 'банан', 'груша' ];
+const fruitsSort = fruits.sort();
+
+console.log(fruits);
+document.write('<ul>');
+for(let i = 0; i < fruitsSort.length; i++){
+    document.write('<li>');
+    document.write(fruitsSort[i]);
+    document.write('</li>');
+}
+document.write('</ul>');
