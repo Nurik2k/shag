2594f5f483d0ec010febc22e979b38041dff2797
Nurik <nurkworld534@gmail.com>
2022-09-09 19:40:00 +0600

    start lesson

---
+++ JSBRAT/lesson1/script.js
@@ -0,0 +1,14 @@
+// let array = ['1', '2', '3'];
+// let newArray = [...array];
+// console.log(array);
+// newArray.push('4');
+// console.log(array);
+// console.log(newArray);
+
+
+// let string = 'Моя строка';
+// let newString = string;
+// console.log(string);
+// newString += 'не изменилась!';
+// console.log(newString);
+
