ca39ed48efb0dc6658f3d9fc836df663e3e6096b
Nurik <nurkworld534@gmail.com>
2022-09-09 21:30:44 +0600

    Hello...

--- JSBRAT/lesson1/script.js
+++ JSBRAT/lesson1/script.js
@@ -18,4 +18,54 @@
 // let string = 'Сьешь еще этих мягких французских булок, да выпей чаю.';
 // console.log(string.split(' '));//из строки в массив
 
+// let string = '      +7 (777) 777 - 77 - 77      ';
+// let cleanString = string.trim();
+// console.log(string);
+// console.log(cleanString);
 
+
+// let string1 = 'cьешь еще этих мягких французских булок, ';
+// function upperFirst(str){
+    
+// console.log(`${ str.substring(0,1).toUpperCase()} ${str.substring(1)}`);
+// }
+// upperFirst(string1);
+
+// let string = 'cьешь еще этих мягких французских булок';
+// const glasnye = ['а', 'е', 'у', 'о', 'и', 'э', 'ы', 'ю', 'я'];
+// function glasnyCount(){
+//     let count = 0;
+//     for(let i = 0; i < string.length; i++){
+//         for(let j = 0; j < glasnye.length; j++) {
+//             if(string[i] === glasnye[j]) {
+//             count += 1;
+//             }
+//         }
+//     }
+//     console.log(count);
+// }
+// glasnyCount();
+
+
+// let string = 'ТОЛЬКО СЕГОДНЯ! Распродажа! 2 по цене одного! Торопись!';
+// const glasnye = ['100% бесплатно', 'увеличение продаж', 'только сегодня', 'не удаляйте', 'ххх'];
+// function glasnyCount(str){
+//     let result = false;
+//     let temp = str.toLowerCase();
+//     for(let i = 0; i < string.length; i++){
+//             if(temp.indexOf(glasnye[i]) !== -1) {
+//                 result = true;
+//             }
+//     }
+//     console.log(result);
+// }
+// glasnyCount(string);//true
+
+
+let string = 'ТОЛЬКО СЕГОДНЯ! Распродажа! 2 по цене одного! Торопись!';
+function truncate(str, count){
+    if(str.length > count){
+        return `${str.substring(0, count -3)}...`
+    }
+}
+console.log(truncate('Hello, world!', 8));
