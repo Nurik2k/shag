b8f1c5011e4c064a8192b6b3673068f810fbd147
Nurik <nurkworld534@gmail.com>
2022-09-10 17:47:59 +0600

    Task 1

---
+++ JSBRAT/DZ/script.js
@@ -0,0 +1,18 @@
+// let number1 = parseInt(prompt("Введите первое число: "));
+// let number2 = parseInt(prompt("Введите второе число: "));
+// let result= 0;
+// if(number1 < number2){
+//     while(number1 <= number2){
+//         result += number1;
+//         number1++;
+//     }
+// }
+// else{
+//     while(number1 >= number2){
+//         result += number2;
+//         number2++;
+//     }
+// }
+// console.log(`Сумма всех чисел: ${result}`);
+//________________________________________________________________________
+
