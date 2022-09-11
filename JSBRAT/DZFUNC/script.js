14ce9a7bea346ae0368827e2f602776efa43baed
Nurik <nurkworld534@gmail.com>
2022-09-11 15:17:49 +0600

    Task 1

---
+++ JSBRAT/DZFUNC/script.js
@@ -0,0 +1,14 @@
+// function equals(num1, num2){
+// if(num1 > num2){
+//     alert('1');
+// }
+// else if(num1 < num2){
+//     alert('-1');
+// }
+// else if(num1 === num2){
+//     alert('0');
+// }
+// }
+// let number1 = prompt("Введите первое число: ");
+// let number2 = prompt("Введите второе число: ");
+// equals(number1, number2);
