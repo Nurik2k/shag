cfe3a28dc4278eefb436af1c680fe65490580428
Nurik <nurkworld534@gmail.com>
2022-09-25 19:09:48 +0600

    DZ

---
+++ csharp/CDZ/CDZ/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDZ
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Array array = new Array(10);
+            array[5] = 4;
+            Console.WriteLine(array[5]);
+            
+
+        }
+    }
+    public class Array
+    {
+        int[] arr;
+
+        public Array(int size) => arr = new int[size];
+
+        public int this[int index]
+        {
+            get { return arr[index]; }
+            set { arr[index] = value * value; }
+        }
+    }
+}
