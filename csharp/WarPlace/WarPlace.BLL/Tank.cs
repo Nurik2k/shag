473a46ef20c6f31d5c6696f9bfb168125dc3fdf5
Nurik <nurkworld534@gmail.com>
2022-09-25 19:01:24 +0600

    fuhh

---
+++ csharp/WarPlace/WarPlace.BLL/Tank.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarPlace.BLL
+{
+    public abstract class Tank
+    {
+        static Random random = new Random();
+        int Ammo = random.Next(0, 100);
+        int Hp = random.Next(0, 100);
+        int Maneuverability = random.Next(0, 100);
+
+        // В данной логике не учитывается что параметры могут быть одинаковые
+        public static void Ataka(TankT34 t34, TankPantera pantera)
+        {
+            int pointT34 = 0, pointPantera = 0;
+
+            if (t34.Ammo > pantera.Ammo) pointT34++;
+            else pointPantera++;
+
+            if (t34.Hp > pantera.Hp) pointT34++;
+            else pointPantera++;
+
+            if (t34.Maneuverability > pantera.Maneuverability) pointT34++;
+            else pointPantera++;
+
+            
+            
+                
+                
+            
+        }
+    }
+
+  
+}
