9e80285e510cd1d759e3d2b50ad666910bd622e6
Nurik <nurkworld534@gmail.com>
2022-10-08 17:59:55 +0600

    dztrue

---
+++ csharp/DZModule04/DZModule04/Car.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZModule04
+{
+    partial class Car
+    {
+        public int Milage
+        {
+            get { return milage; }
+            set { milage = value; }
+        }
+        public string Mark
+        {
+            get { return mark; }
+            set { mark = value; }
+        }
+        public bool Gasoline
+        {
+            get { return gasoline; }
+            set { gasoline = value; }
+        }
+        public int EnginePower
+        {
+            get { return enginePower; }
+            set { enginePower = value; }
+        }
+        public int MaxSpeed
+        {
+            get { return maxSpeed; }
+            set { maxSpeed = value; }
+        }
+
+        private int milage;
+        private string mark;
+        private bool gasoline;
+        private int enginePower;
+        public int maxSpeed;
+
+        public static string carForeign;
+        public static int YearOfIssueNotLower;
+
+
+        public Car()
+        {
+            milage = 0;
+            mark = "default";
+            gasoline = false;
+            enginePower = 0;
+            maxSpeed = 0;
+            //Print();
+        }
+        public Car(int _milage, string _mark, bool _Gasoline, int _enginePower, int _maxSpeed)
+        {
+            milage = _milage;
+            mark = _mark;
+            gasoline = _Gasoline;
+            enginePower = _enginePower;
+            maxSpeed = _maxSpeed;
+            Print();
+        }
+        public Car(string _mark)
+        {
+            mark = _mark;
+            Print();
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("milage = " + milage);
+            Console.WriteLine("mark = " + mark);
+            Console.WriteLine("Gasoline = " + gasoline);
+            Console.WriteLine("enginePower = " + enginePower);
+            Console.WriteLine("maxSpeed = " + maxSpeed);
+        }
+        public void Print(ref Car obj)
+        {
+            Console.WriteLine("milage = " + obj.milage);
+            Console.WriteLine("mark = " + obj.mark);
+            Console.WriteLine("Gasoline = " + obj.gasoline);
+            Console.WriteLine("enginePower = " + obj.enginePower);
+            Console.WriteLine("maxSpeed = " + obj.maxSpeed);
+        }
+        static Car()
+        {
+            carForeign = "carEurope";
+            YearOfIssueNotLower = 1990;
+        }
+    }
+}
---
+++ csharp/DZModule04/DZModule04/Program.cs
@@ -0,0 +1,34 @@
+﻿using DZModule04;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace case_1
+{
+
+    class Program : Car
+    {
+        static void Main(string[] args)
+        {
+            Car Car1 = new Car();
+            Car Car2 = new Car("reno");
+            Car[] car3Cars = new Car[5];
+            for (int i = 0; i < car3Cars.Length; i++)
+            {
+                car3Cars[i] = new Car();
+            }
+            
+            //Car2.Print();
+            Console.WriteLine();
+            Car1.maxSpeed = 9;
+            Console.WriteLine(Car1.maxSpeed);
+            Car1.Print(ref Car1);
+            Console.WriteLine();
+            Car1.Print();
+            Console.WriteLine();
+            Console.WriteLine(Car.carForeign);
+        }
+    }
+}
