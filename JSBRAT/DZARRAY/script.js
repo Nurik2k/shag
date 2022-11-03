3c17495efb4fb35235e3b3cec00d82b8cc20ee29
Nurik <nurkworld534@gmail.com>
2022-11-03 21:35:33 +0600

    End

--- JSBRAT/DZARRAY/script.js
+++ JSBRAT/DZARRAY/script.js
@@ -63,38 +63,160 @@
 // showProductList(ProductList);
 //__________________________________________________________
 
-$('#print_check').click(function() {
-    let arr = [{
-        name: 'Apple',
-        bill: parseInt('230')
-    }, {
-        name: 'Potato',
-        bill: parseInt('80')
-    }, {
-        name: 'Banana',
-        bill: parseInt('50')
-    }, ];
-    
-        
-  GetCheck(arr);
-});
+
+// let checks = [{name:'milk',qnty:'2',priceFor1:'3.0'},{name:'bred',qnty:'3',priceFor1:'1.2'},{name:'butter',qnty:'1',priceFor1:'4.5'}];
+
+// function show()
+// {
+//     for(let i=0;i<checks.length;i++)
+//     {
+//         console.log(`${checks[i].name}-${checks[i].qnty}-${checks[i].priceFor1}`);
+//     }
+// }
+
+// show();
+
+// function summ(arr)
+// {
+//     let s=0;
+//     for(let i=0;i<arr.length;i++)
+//     {
+//         s+=parseInt(arr[i].qnty)*parseFloat(arr[i].priceFor1);
+//     }
+//     console.log(`общая сумма покупки ${s}`);
+// }
+// summ(checks);
+
+// function mostExpensive(arr)
+// {
+//     let max=parseInt(arr[0].qnty)*parseFloat(arr[0].priceFor1);
+//     let ind =0;
+//     for(let i=1;i<arr.length;i++)
+//     {
+//         if(max<parseInt(arr[i].qnty)*parseFloat(arr[i].priceFor1))
+//         {
+//             max=parseInt(arr[i].qnty)*parseFloat(arr[i].priceFor1);
+//             ind=i;
+//         }
+//     }
+//     console.log(`самая дорогая покупка в чеке ${max}`);
+//     console.log(`${checks[ind].name}-${checks[ind].qnty}-${checks[ind].priceFor1} = ${max}`);
+// }
+// mostExpensive(checks);
+
+// function averagePrice(arr)
+// {
+//     let overall=0;
+//     let overallQuantity = 0;
+//     for(let i=0;i<arr.length;i++)
+//     {
+//         overall+=parseInt(arr[i].qnty)*parseFloat(arr[i].priceFor1);
+//         overallQuantity+=parseInt(arr[i].qnty);
+//     }
+//     console.log(`средняя стоимость одного товара в чеке - ${overall/overallQuantity}`);
+// }
+
+// averagePrice(checks);
+//_________________________________________________________________________________________
 
 
-  function GetCheck(arr){
+// let massv = [{name:'style1',fontSize:'120%'},{name:'style1',color:'red'},{name:'style1',textAlign:'center'},{name:'style1',underline:'1'}];
 
-    document.write('<ol start="1">');
-    arr.forEach(ar => {
-        document.write(<li> <span style="color: red;">${ar.name}</span> ${ar.bill} tenge.</li>);
-    });
-    document.write('<ol>'); 
-    AllCountPrice(arr);
-  }
-  function AllCountPrice(arr){
-    let sum = 0;
-    for(let i = 0; i<arr.lenght; i++){
-        sum += arr.bill[i++];
+// let temp = 'HEHEHEHHEHEH';
+// function showTextWithStyle(mass,txt)
+// {
+//     document.write(`<p style=" font-size: ${mass[0].fontSize}; text-align: ${mass[2].textAlign}; color: ${mass[1].color}; underline: ${mass[3].underline};">  ${txt} </p>`);
+// }
+// showTextWithStyle(massv,temp);
+//____________________________________________________________________________________________
+
+
+class Rooms
+{
+    constructor(numb,cpcty,fclty)
+    {
+        this.name=numb;
+        this.cpcty=cpcty
+        this.fclty=fclty;
     }
-    document.write(sum);
 }
 
+let masOfRooms = new Array();
 
+function printRooms()
+{
+    console.log(masOfRooms.length);
+    for(let i=0;i<masOfRooms.length;i++)
+    {
+        console.log(masOfRooms[i]);
+    }
+}
+
+function showRoomsFclty(fclty001)
+{
+    for(let i=0;i<masOfRooms.length;i++)
+    {
+        if(masOfRooms[i].fclty===fclty001)
+            console.log(masOfRooms[i]);
+    }
+}
+
+class Group
+{
+    constructor(name,qtty,fclty)
+    {
+        this.name=name;
+        this.qtty=qtty
+        this.fclty=fclty;
+    }
+}
+
+let sep2122 = new Group('sep2122',14,'sep');
+
+function showRoomsForGroup(group)
+{
+    for(let i=0;i<masOfRooms.length;i++)
+    {
+        if(masOfRooms[i].cpcty>=group.qtty)
+            console.log(masOfRooms[i]);
+    }
+}
+
+function sortByCapty()
+{
+    masOfRooms.sort(function(a, b) {
+        return a.cpcty - b.cpcty;
+      });
+}
+
+function sortByAlpha()
+{
+    masOfRooms.sort(function(a, b) {
+    var nameA=a.name.toLowerCase(), nameB=b.name.toLowerCase();
+    if (nameA < nameB) return -1;//сортируем строки по возрастанию  
+    if (nameA > nameB)  return 1;
+    return 0; // Никакой сортировки
+    });
+}
+
+let room223 = new Rooms('Aroom223',20,'sep');
+let room220 = new Rooms('Broom220',10,'sep');
+let room210 = new Rooms('Froom210',50,'gts');
+
+masOfRooms.unshift(room210);
+masOfRooms.unshift(room220);   
+masOfRooms.unshift(room223);
+
+printRooms();
+
+showRoomsFclty('gts');
+
+showRoomsForGroup(sep2122);
+
+sortByCapty();
+
+printRooms();
+
+sortByAlpha();
+
+printRooms();
