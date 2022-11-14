2d19e490461c6e17fdd45ecf1cf9637f15939bf1
Nurik <nurkworld534@gmail.com>
2022-11-14 21:47:03 +0600

    Sign In

--- ADO.NET/Finaldb/Finaldb/Program.cs
+++ ADO.NET/Finaldb/Finaldb/Program.cs
@@ -3,20 +3,28 @@ using Finaldb.Data;
 using Finaldb.Models;
 using System.Collections;
 using System.Security.Cryptography;
-
-
+using System.Text;
+using System;
+using System.Data.Common;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Runtime.CompilerServices;
 
 namespace FinalDb
 {
     internal class Program
     {
+        const string ConnectionString = "Server=NURIK;Database=ChatDb;Trusted_Connection=true;Encrypt=false";
         static void Main(string[] args)
         {
             using var DbContext = new ChatDbContext();
             try
             {
-                Console.WriteLine("Выберите пункт меню:");
-                Console.WriteLine("1. Регистрация");
+                bool isDebug = true;
+
+                Console.WriteLine("Select menu item:");
+                Console.WriteLine("1. Registration");
+                Console.WriteLine("2. Sign in");
                 int ch = Int32.Parse(Console.ReadLine());
                 switch (ch)
                 {
@@ -24,10 +32,20 @@ namespace FinalDb
                         Registration(DbContext);
                         break;
                     case 2:
-                        HashPass();
+                        string login = "", password = "";
+                        Console.Write("Enter login: ");
+                        
+                            login = Console.ReadLine();
+                            Console.Write("Enter password: ");
+                            password = Console.ReadLine();
+                        
+                        
+
+                        SignIn(login, password);
                         break;
 
                 }
+
             }
             catch (Exception ex)
             {
@@ -37,24 +55,62 @@ namespace FinalDb
 
         static void Registration(ChatDbContext db)
         {
-            User newUser = new User();
-            Console.Write("Введите логин: ");
-            newUser.Login = Console.ReadLine();
-            
-            db.Users.Add(newUser);
-            db.SaveChanges();
-            Console.WriteLine("Успешно добавлено!");
+            try
+            {
+                User newUser = new User();
+                Console.Write("Enter login: ");
+                newUser.Login = Console.ReadLine();
+                Console.Write("Enter password: ");
+                newUser.Password = HashPass(newUser.Password = Console.ReadLine());
+                db.Users.Add(newUser);
+                db.SaveChanges();
+                Console.WriteLine("Added!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
-        static void HashPass()
+        static string HashPass(string pass)
         {
-            User newUser = new User();
-            Console.Write("Введите пароль: ");
-            newUser.Password = Console.ReadLine();
-            Hashtable hashtable= new Hashtable();
-            hashtable.Add(newUser.Password, newUser);
-            Console.WriteLine(hashtable);
+            var md5 = MD5.Create();
+            var hash = md5.ComputeHash(
+                Encoding.UTF8.GetBytes(pass)
+            );
+            return Convert.ToBase64String(hash);
         }
-       
+        static bool SignIn(string login, string password)
+        {
+            try
+            {
+                const string SqlQuery = "SELECT [login], [password] FROM dbo.Users WHERE login = @Login";
+                using var SqlConnection = new SqlConnection(ConnectionString);
+                SqlConnection.Open();
+
+                SqlCommand cmd = new SqlCommand(SqlQuery, SqlConnection);
+                cmd.Parameters.Add("Login", SqlDbType.VarChar, 500).Value = login;
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    var pass = reader.GetString(1);
+                    if (HashPass(password) == reader["password"].ToString())
+                    {
+                        Console.WriteLine("Wellcome!");
+                        return true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Password is wrong!");
+                        return false;
+                    }
+                }
+                return false;
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); return false; }
+
+
+        }
     }
 }
 
