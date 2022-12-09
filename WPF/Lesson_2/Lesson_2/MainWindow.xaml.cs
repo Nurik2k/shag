fde435ec03110b5ff9236c228c28aeb90ee5e263
Nurik2k <nurkworld534@gmail.com>
2022-12-09 21:53:53 +0600

    End

---
+++ WPF/Lesson_2/Lesson_2/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lesson_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //stMain.Margin = new Thickness(10, 10, 55, 5);
+        }
+
+        private void btnGenerateQrCode_Click(object sender, RoutedEventArgs e)
+        {
+            //var rb = rdSize.Children;
+            int size = 0;
+            foreach (var item in rdSize.Children)
+            {
+                RadioButton rb = (RadioButton)item;
+                if ((bool)rb.IsChecked)
+                {
+                    size = Convert.ToInt32(rb.Content);
+                    break;
+                }
+            }
+            RadioButton rb_ = rdSize.Children.Cast<RadioButton>().FirstOrDefault(item=>(bool)item.IsChecked);
+
+            var restClient = new RestClient("http://qrcoder.ru/code");
+            var request = new RestRequest(String.Format("?{0}&{1}&0", tbxQrText.Text, size), Method.Get);
+            RestResponse response = restClient.Execute(request);
+            var img = response.RawBytes;
+
+            WindowQrCodeImage wqci = new WindowQrCodeImage(img, 290, 290);
+            wqci.Show();
+        }
+    }
+}
---
+++ WPF/Lesson_2/Lesson_2/WindowQrCodeImage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lesson_2
+{
+    /// <summary>
+    /// Interaction logic for WindowQrCodeImage.xaml
+    /// </summary>
+    public partial class WindowQrCodeImage : Window
+    {
+        public WindowQrCodeImage(byte[] buffer, int height, int width)
+        {
+            InitializeComponent();
+            GetImage(buffer, qrImage);
+            this.Height= height;
+            this.Width= width;
+        }
+        
+            private void GetImage(byte[] rawBytes, Image img)
+            {
+                byte[] imageBytes = rawBytes;
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = new MemoryStream(imageBytes);
+                bitmapImage.CreateOptions = BitmapCreateOptions.None;
+                bitmapImage.CacheOption = BitmapCacheOption.Default;
+                bitmapImage.EndInit();
+
+                #region Save img
+                //JpegBitmapEncoder encoder = new JpegBitmapEncoder();
+                //Guid photoID = System.Guid.NewGuid();
+                //String photolocation = String.Format(@"c:\temp\{0}.jpg", Guid.NewGuid().ToString());
+                //encoder.Frames.Add(BitmapFrame.Create(bitmapImage));
+                //using (var filestream = new FileStream(photolocation, FileMode.Create))
+                //    encoder.Save(filestream);
+                #endregion
+
+                this.Dispatcher.Invoke((Action)(() => { img.Source = bitmapImage; }));
+            }
+        
+    }
+}
