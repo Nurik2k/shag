3f55a28c7ad022718e552a519539829af41235ce
Nurik2k <nurkworld534@gmail.com>
2022-12-16 20:16:02 +0600

    Edited

--- WPF/DZ1/DZ1/MainWindow.xaml.cs
+++ WPF/DZ1/DZ1/MainWindow.xaml.cs
@@ -12,17 +12,42 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace DZ1
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void CreateXML_Click(object sender, RoutedEventArgs e)
+        {
+            using var reader = XmlReader.Create("https://habrahabr.ru/rss/interesting/%22");
+
+        }
+
+        private void btnInfo_Click(object sender, RoutedEventArgs e)
+        {
+            using var reader = XmlReader.Create("https://habrahabr.ru/rss/interesting/%22");
+
+            reader.ReadToFollowing("title");
+            labelTitle.Content += ": " + reader.ReadElementContentAsString();
+            reader.ReadToFollowing("description");
+            labelDescription.Content += ": " + reader.ReadElementContentAsString();
+            reader.ReadToFollowing("managingEditor");
+            labelManagingEditor.Content += ": " + reader.ReadElementContentAsString();
+            reader.ReadToFollowing("generator");
+            labelGenerator.Content += ": " + reader.ReadElementContentAsString();
+            reader.ReadToFollowing("pubDate");
+            labelPubdate.Content += " " + reader.ReadElementContentAsString();
         }
     }
 }
