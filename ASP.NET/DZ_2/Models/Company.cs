8737d22c45956c9f302bd6a71c963ecd547e1f3c
Nurik2k <nurkworld534@gmail.com>
2023-02-25 18:44:11 +0600

    Finish dz2

---
+++ ASP.NET/DZ_2/Models/Company.cs
@@ -0,0 +1,8 @@
+﻿namespace DZ_2.Models
+{
+    public class Company
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ ASP.NET/DZ_2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DZ_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
