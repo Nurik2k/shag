2d17d05f71c9c804cc944d53eee844429e8287a6
Nurik2k <nurkworld534@gmail.com>
2023-04-03 21:13:17 +0600

    SimpleRoute

---
+++ ASP.NET/SimpleRouting/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleRouting.Models;
+using System.Diagnostics;
+
+namespace SimpleRouting.Controllers
+{
+    [Route("main")]// /home
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        //[Route("[action]/{id:int}/{name:maxlength(10)}")]
+        [Route("Home/Index/{id?}")]// /home/Home/Index
+        public IActionResult Index(int id, string name)
+        {
+            ViewBag.Id = id;
+            ViewBag.Name = name;
+            return View();
+        }
+
+        [Route("[controller]/[action]")]
+        public IActionResult Privacy(string data)
+        {
+            var controller = RouteData.Values["controller"].ToString();
+            var action = RouteData.Values["action"].ToString();
+            //return View();
+            return Content($"controller: {controller} | action: {action}");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ASP.NET/SimpleRouting/Controllers/InvoicesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleRouting.Controllers
+{
+    public class InvoicesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult View(string number)
+        {
+            ViewBag.Number = number;
+            return View();
+        }
+    }
+}
