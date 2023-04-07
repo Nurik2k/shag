076151fa470dbf0c13e4050336bf0345c80874ad
Nurik2k <nurkworld534@gmail.com>
2023-04-07 21:46:41 +0600

    Culture

--- ASP.NET/SimpleRouting/Controllers/HomeController.cs
+++ ASP.NET/SimpleRouting/Controllers/HomeController.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using SimpleRouting.Models;
 using System.Diagnostics;
 
 namespace SimpleRouting.Controllers
 {
-    [Route("Home")]
-    [Route("main")]
+    //[Route("Home")]
+    //[Route("main")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -17,20 +18,37 @@ namespace SimpleRouting.Controllers
 
         //[Route("[action]/{id:int}/{name:maxlength(10)}")]
         //[Route("Home/Index/{id?}")]// /Route/Home/Index
-        [Route("index/{id?}")]
+        //[Route("index/{id?}")]
         public IActionResult Index(int id, string name)
         {
-            ViewBag.Id = id;
-            ViewBag.Name = name;
+            //CookieOptions options = new CookieOptions();
+            //options.Expires = DateTimeOffset.Now.AddMilliseconds(10);
+            //Response.Cookies.Append("testCookies", "666", options);
+
+            //string test = Request.Cookies["testCookies"];
+            //Response.Cookies.Delete("testCookies");
+
+            HttpContext.Session.SetString("product", "PenDrive");
+            string sessionValue = "";
+            if(HttpContext.Session != null)
+            {
+                sessionValue = HttpContext.Session.GetString("product");
+                if (string.IsNullOrWhiteSpace(sessionValue)) sessionValue = "Session timed out";
+            }
+            ViewBag.Id = sessionValue;
+            //ViewBag.Name = name;
             return View();
         }
 
         [HttpGet]
         //[Route("Sunny")]
         //[Route("CheckPrivacy")]
+        //[Route("New/[controller]/Almaty/[action]")]
         //[Route("[controller]/[action]")]
         //[Route("/PrivacyPage")]
         //[Route(" ", Name = "Privac")]
+        //[Route("[action]")]
+        //[Route("AboutUs")]
         public IActionResult Privacy(string data)
         {
             var controller = RouteData.Values["controller"].ToString();
--- ASP.NET/SimpleRouting/Program.cs
+++ ASP.NET/SimpleRouting/Program.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Routing.Constraints;
-using Microsoft.Extensions.FileSystemGlobbing;
-using SimpleRouting.Models;
-using System.ComponentModel.DataAnnotations;
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.Cookie.Name = "MyApp.Session";
+    options.IdleTimeout = TimeSpan.FromSeconds(10);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+builder.Services.AddMvc().AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix);
+builder.Services.Configure<RequestLocalizationOptions>(option =>
+{
+    var supportedCulture = new[] {
+    new CultureInfo("en-US"),
+    new CultureInfo("ru-Ru"),
+    new CultureInfo("kk-KZ")
+    };
+    option.DefaultRequestCulture = new RequestCulture(culture: "kk-KZ", uiCulture: "kk-Kz");
+    option.SupportedCultures= supportedCulture;
+    option.SupportedUICultures= supportedCulture;
+});
 
 var app = builder.Build();
 
@@ -15,56 +35,62 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+
+app.UseSession();
+
 app.UseStaticFiles();
 
 app.UseRouting();
 
+var LocOptions = app.Services.GetRequiredService<IOptions<RequestLocalizationOptions>>();
+app.UseRequestLocalization(LocOptions.Value);
+
 app.UseAuthorization();
 
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllerRoute(
-        name: "default",
-        pattern: "{controller}/{action}",
-        //значение по умолчанию для параметров маршрута
-        defaults: new
-        {
-            controller = "Home",
-            action = "Index",
-            //HttpMethod = new HttpMethodRouteConstraint("GET")
-            customeConstraint = new UserAgentConstraint("Chrome")
-        },
-        constraints: new
-        {
-            id = new IntRouteConstraint()
-        });
+//app.UseEndpoints(endpoints =>
+//{
+//    endpoints.MapControllerRoute(
+//        name: "default",
+//        pattern: "{controller}/{action}",
+//        //значение по умолчанию для параметров маршрута
+//        defaults: new
+//        {
+//            controller = "Home",
+//            action = "Index",
+//            //HttpMethod = new HttpMethodRouteConstraint("GET")
+//            customeConstraint = new UserAgentConstraint("Chrome")
+//        },
+//        constraints: new
+//        {
+//            id = new IntRouteConstraint()
+//        });
 
-    endpoints.MapControllerRoute(
-        name: "Documents",
-        pattern: "documents/{controller}/{number}/{action}",
-        //значение по умолчанию для параметров маршрута
-        defaults: new
-        {
-            controller = "Invoices",
-            action = "View",
-        },
-        constraints: new
-        {
-            number = new RegexRouteConstraint("[a-z]{2}")
-        });
-});
+//    endpoints.MapControllerRoute(
+//        name: "Documents",
+//        pattern: "documents/{controller}/{number}/{action}",
+//        //значение по умолчанию для параметров маршрута
+//        defaults: new
+//        {
+//            controller = "Invoices",
+//            action = "View",
+//        },
+//        constraints: new
+//        {
+//            number = new RegexRouteConstraint("[a-z]{2}")
+//        });
+//});
 
-app.MapControllerRoute(
-    name: "CatchAll",
-    pattern: "{controller=Home}/{action=Index}/{*data}");
+//app.MapControllerRoute(
+//    name: "CatchAll",
+//    pattern: "{controller=Home}/{action=Index}/{*data}");
 
-app.MapControllerRoute(
-    name: "twoParametrRoute",
-    pattern: "{controller=Home}/{action=Privacy}/{x}/{y}");
+//app.MapControllerRoute(
+//    name: "twoParametrRoute",
+//    pattern: "{controller=Home}/{action=Privacy}/{x}/{y}");
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "api/{controller=Home}/{action=Index}");
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "api/{controller=Home}/{action=Index}");
 
 //Определение route template
 app.MapControllerRoute(
@@ -72,10 +98,19 @@ app.MapControllerRoute(
                     //route template для запросов состоящих из двух сегментов
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+//app.MapControllerRoute(
+//    name: "defaultApi",
+//    pattern: "api/{action}",
+//    defaults: new { controller = "Home" });
+
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Index}");
+
 //если мы выстраиваем маршрутизатор только на основе атрибутов
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllers();//нет определенных маршрутов
-});
+//app.UseEndpoints(endpoints =>
+//{
+//    endpoints.MapControllers();//нет определенных маршрутов
+//});
 
 app.Run();
