69fab87394dbe47559d601753bf183cfe0a9b4fb
Nurik2k <nurkworld534@gmail.com>
2023-05-03 20:40:10 +0600

    New

---
+++ SystemProgramming/01 - êß»«½8º«óá¡¿Ñ p¡áß½Ññ«óá¡¡«ú« »a«úaá¼¼¡«ú« ¬«ñá/UploadToExcel/UploadToExcel/Program.cs
@@ -0,0 +1,29 @@
+﻿using Excel = Microsoft.Office.Interop.Excel;
+
+//Объявляем приложение
+Excel.Application ex = new Excel.Application();
+
+//Отобразить Excel
+ex.Visible = true;
+
+//Количество листов в рабочей книге
+ex.SheetsInNewWorkbook = 2;
+
+//Добавить рабочую книгу
+Excel.Workbook workBook = ex.Workbooks.Add(Type.Missing);
+
+//Отключить отображение окон с сообщениями
+ex.DisplayAlerts = false;
+
+//Получаем первый лист документа (счет начинается с 1)
+Excel.Worksheet sheet = (Excel.Worksheet)ex.Worksheets.get_Item(1);
+
+//Название листа (вкладки снизу)
+sheet.Name = "My data";
+
+//Пример заполнения ячеек
+for (int i = 1; i <= 9; i++)
+    for (int j = 1; j < 9; j++)
+        sheet.Cells[i, j] = string.Format("Boom {0} {1}", i, j);
+
+Console.WriteLine("Data uploaded");
---
+++ SystemProgramming/WorkWithExcel/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+using Excel = Microsoft.Office.Interop.Excel;
+Excel.Application ex = new Microsoft.Office.Interop.Excel.Application();
+ex.Visible = true; ex.SheetsInNewWorkbook = 1;
+Excel.Workbook workBook = ex.Workbooks.Add(Type.Missing); ex.DisplayAlerts = false;
+Excel.Worksheet sheet = (Excel.Worksheet)ex.Worksheets.get_Item(1); sheet.Name = "Chisla";
+for (int i = 1; i <= 3; i++)
+{
+    for (int j = 1; j <= 3; j++)
+    {
+        int res = new Random().Next(1, 9); sheet.Cells[i, j] = string.Format("{0}", res);
+    }
+}
