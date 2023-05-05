2c7032f272c1a3a75ee2efb6770b17f77f6ed30f
Nurik2k <nurkworld534@gmail.com>
2023-05-05 19:10:00 +0600

    Sort

---
+++ SystemProgramming/HomeWorks/HW1/HomeWork_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO.MemoryMappedFiles;
+
+string path = "D:/shag/SystemProgramming/HomeWorks/HW1/airports.json";
+
+
+//using (var mmf = MemoryMappedFile.CreateFromFile(path, FileMode.Open))
+//using (var stream = mmf.CreateViewStream())
+//using (var reader = new StreamReader(stream))
+//{
+//    string line;
+
+//    while ((line = reader.ReadLine()) != null)
+//    {
+//        // Обработка строки
+//        Console.WriteLine(line);
+//        Thread.Sleep(100);
+//    }
+//}
+
+//using (var fileStream = new FileStream(path, FileMode.Open, FileAccess.Read))
+//using (var streamReader = new StreamReader(fileStream))
+//{
+//    string line;
+//    while ((line = streamReader.ReadLine()) != null)
+//    {
+//        // Обработка строки
+//        Console.WriteLine(line);
+//        Thread.Sleep(100);
+//    }
+//}
+
+
