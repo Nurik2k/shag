de767130cb10aaeb2bc3f415b69f46f021ced13d
Nurik2k <nurkworld534@gmail.com>
2023-07-03 20:17:42 +0600

    Python statr

--- SystemProgramming/Geometry/Program.cs
+++ SystemProgramming/Geometry/Program.cs
@@ -1,16 +1,20 @@
-﻿using Geometry.dll;
+﻿using Geometry;
+using Geometry.dll;
 using System.Diagnostics;
 
-int a = 4;
+TestGeometry testGeometry = new TestGeometry();
+
+float a = 4;
 int b = 5;
+float h = 6;
 
-Debug.Assert(Rectangle_.Rectangle_Area(a, b) == 20);
-Debug.Assert(Square.Square_Area(a) == 16);
-Debug.Assert(Triangle.Triangle_Area(a, b) == 10);
+testGeometry.Test_Rectangle((int)a, b);
+testGeometry.TestSquare((int)a);
+testGeometry.TestTriangle(a, h);
 
 
-Console.WriteLine("Rectangle areas: " + Rectangle_.Rectangle_Area(a, b));
+Console.WriteLine("Rectangle areas: " + Rectangle_.Rectangle_Area((int)a, b));
 
-Console.WriteLine("Square areas: " + Square.Square_Area(a));
+Console.WriteLine("Square areas: " + Square.Square_Area((int)a));
 
-Console.WriteLine("Triangle areas: " + Triangle.Triangle_Area(a, b));
+Console.WriteLine("Triangle areas: " + Triangle.Triangle_Area(a, h));
---
+++ SystemProgramming/Geometry/TestGeometry.cs
@@ -0,0 +1,26 @@
+﻿using Geometry.dll;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry
+{
+    public class TestGeometry
+    {
+        public void TestTriangle(float a, float h)
+        {
+            Debug.Assert(Triangle.Triangle_Area(a, h) == 12);
+        }
+        public void TestSquare(int a)
+        {
+            Debug.Assert(Square.Square_Area(a) == 16);
+        }
+        public void Test_Rectangle(int a, int h)
+        {
+            Debug.Assert(Rectangle_.Rectangle_Area(a, h) == 20);
+        }
+    }
+}
