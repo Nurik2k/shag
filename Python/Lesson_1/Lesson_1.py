fdb740f4c1ef308d37fbddd3a87ecf0299bab379
Nurik2k <nurkworld534@gmail.com>
2023-07-03 21:47:08 +0600

    DZ1 end

---
+++ Python/Lesson_1/Lesson_1.py
@@ -0,0 +1,26 @@
+#a = int(input())
+#b = int(input())
+#c = int(input())
+
+#result = (a + b + c) // 3
+#print(result)
+#_______________________________
+
+#salary = int(input())
+#print("Salary: ", salary)
+#print("bank loan monthly payment:", 2000)
+#salary -= 2000
+#print("utility bills", 1000)
+#salary-=1000
+#print("Remainder: ", salary)
+#___________________________________________
+
+#a = int(input())
+#H = int(input())
+#print("Rhombus area: ", a * H)
+#__________________________________________
+
+#print("To be","or not", "to be", sep="\n")
+#________________________________________________
+
+print("Life is what happens","\n\twhen", "\n\t\tyou are busy making other plans", "\n\t\t\t\t\t\tJohn Lennon")
