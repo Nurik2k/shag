8b85b489223a2043ce45e5188ca54b7ea6626eb1
Nurik2k <nurkworld534@gmail.com>
2023-07-14 21:34:51 +0600

    Calculator end

--- Python/Lesson3/calculator.py
+++ Python/Lesson3/calculator.py
@@ -1,23 +1,73 @@
+menu = [
+    ["Select an operation:"],
+    ["1. Addition"],
+    ["2. Substraction"],
+    ["3. Multiplication"],
+    ["4. Division"],
+    ["5. Power"],
+    ["6. Square root"], 
+    ["7. Logarithm"],
+    ["8. Factorial"],
+    ["0. Exit"]
+]
 
-def calculate(a, operator, b):
-    match operator:
-        case '+':
-            result = a + b
-        case '-':
-            result = a - b
-        case '*':
-            result = a * b
-        case '/':
-            result = a / b
-        case _:
-            print("Invalid operator")
-            return
-    print(result)
+def Addition():
+    num1 = int(input("Enter num1: "))
+    num2 = int(input("Enter num2: "))
+    print("Result: ", num1 + num2, "\n")
+    Menu()
 
+def Substraction():
+    num1 = int(input("Enter num1: "))
+    num2 = int(input("Enter num2: "))
+    print("Result: ", num1 - num2, "\n")
+    Menu()
 
-expression = input().split()
-a = int(expression[0])
-operator = expression[1]
-b = int(expression[2])
-calculate(a, operator, b)
+def Multiplication():
+    num1 = int(input("Enter num1: "))
+    num2 = int(input("Enter num2: "))
+    print("Result: ", num1 * num2, "\n")
+    Menu()
 
+def Division():
+    num1 = int(input("Enter num1: "))
+    num2 = int(input("Enter num2: "))
+    try:
+        result = num1 / num2
+    except ZeroDivisionError:
+        print("Ошибка деления на ноль!")
+    except Exception as e:
+        print("Произошла ошибка: ", str(e))
+    else:
+        print("Результат: ", result, "\n")
+    finally:
+        Menu()
+
+def Menu():
+    for i in menu:
+        for j in i:
+            print(j)
+    print()
+
+    num = int(input())
+    if num <= 8:
+        match num:
+            case 1:
+                Addition()
+            
+            case 2:
+                Substraction()
+        
+            case 3:
+                Multiplication()
+
+            case 4:
+                Division()
+
+            case 0: 
+                print("Выход!")
+    else:
+        print("Нет такой команды!")
+
+if __name__ == "__main__":
+    Menu()
