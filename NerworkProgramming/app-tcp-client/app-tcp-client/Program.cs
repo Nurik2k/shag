755d9248467385f352a09b018b9bd5479e8a7c15
Nurzhan <nurkworld534@gmail.com>
2023-09-11 19:37:41 +0600

    NetworkProgramming

---
+++ NerworkProgramming/app-tcp-client/app-tcp-client/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+try
+{
+    IPAddress address = IPAddress.Parse("127.0.0.1");
+    IPEndPoint endPoint = new IPEndPoint(address, 9595);
+
+    #region Top 3 creating TCP client
+    //Creating TCP client
+    //1) TcpClient client = new TcpClient("127.0.0.1", 9595);
+
+    //2)
+    //TcpClient client = new TcpClient();
+    //client.Connect("127.0.0.1", 9595);
+    //client.Connect(endPoint);
+
+    //3)
+    TcpClient client = new TcpClient(AddressFamily.InterNetwork);
+    IPAddress[] iPAddresses = Dns.GetHostAddresses("127.0.0.1");
+    client.Connect(iPAddresses, 9595);
+    #endregion
+
+    //Will the connection remain open after the socket is closed, and for how long
+    client.LingerState = new LingerOption(false, 0);
+
+    //Settings to collect full data package 
+    client.NoDelay = true;
+
+    #region Comments
+    //Set size an income package
+    //client.ReceiveBufferSize = 1024;
+
+    //Set size an outcome package
+    //client.SendBufferSize = 1024;
+
+    //Set ms time for refuce connection
+    //client.ReceiveTimeout = 10000;
+
+    //Set ms await for send package
+    //client.SendTimeout = 10000;
+    #endregion
+
+    Console.Write("-> ");
+
+    string? message = Console.ReadLine();
+
+    //translate message to bytes
+    byte[] data = Encoding.UTF8.GetBytes(message);
+
+    //Get trhead to read and write
+    NetworkStream stream = client.GetStream();
+
+    //Post message to client
+    stream.Write(data, 0, data.Length);
+    Console.WriteLine();
+
+    //Getting response from server
+    data = new byte[256];
+    string responseData = "";
+
+    //Reading first package server response
+    int bytes = stream.Read(data, 0, data.Length);
+    responseData = Encoding.UTF8.GetString(data, 0, bytes);
+    Console.WriteLine("-> {0}", responseData);
+
+    //Close all
+    stream.Close();
+    client.Close();
+}
+catch (SocketException se)
+{
+    //Only socket exeptions
+    Console.WriteLine(se.Message);
+}
+catch (Exception ex)
+{
+    //All exeptions
+    Console.WriteLine(ex.Message);
+}
---
+++ NerworkProgramming/app-tcp-client/app-tcp-server/Program.cs
@@ -0,0 +1,102 @@
+﻿using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+
+class Main
+{
+    static List<PortInfo> GetOpenPort()
+    {
+        IPGlobalProperties properties = IPGlobalProperties.GetIPGlobalProperties();
+        IPEndPoint[] topEndPoints = properties.GetActiveTcpListeners();
+        TcpConnectionInformation[] tcpConnectionInformation = properties.GetActiveTcpConnections();
+
+        var data = tcpConnectionInformation.Select(s => new PortInfo(
+            s.LocalEndPoint.Port,
+            String.Format("{0}:{1}", s.LocalEndPoint.Address, s.LocalEndPoint.Port),
+            String.Format("{0}:{1}", s.RemoteEndPoint.Address, s.RemoteEndPoint.Port),
+            s.State.ToString()
+            )).ToList();
+
+        return data;
+    }
+
+    public void Server()
+    {
+
+        List<PortInfo> ports = GetOpenPort();
+        foreach (PortInfo item in ports)
+        {
+            Console.WriteLine("Post: {0} - {1}", item.PortNumber, item.State);
+        }
+        return;
+
+        #region TcpListener
+        TcpListener server = null;
+
+        try
+        {
+            //Ping myping = new Ping();
+            //PingReply reply = myping.Send("192.168.111.187", 9595);
+            //Console.WriteLine("Status: {0}\nTime: {0}\nAddress: {0}", reply.Status, reply.RoundtripTime.ToString(), reply.Address);
+
+            IPAddress localAdr = IPAddress.Parse("127.0.0.1");
+            NetworkStream stream = null;
+
+            server = new TcpListener(localAdr, 9595);
+            //Connect with EndPoint and waiting transfering income tryes
+            server.Start();
+
+            byte[] bytes = new byte[1024];
+            string data = "";
+
+            while (true)
+            {
+                //Get waiting respose
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("Connected.");
+
+                stream = client.GetStream();
+                int i = 0;
+                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                {
+                    data = Encoding.UTF8.GetString(bytes, 0, i);
+                    Console.WriteLine("-> {0}", data);
+
+                    data = "Messge Received!";
+                    byte[] receiveData = Encoding.UTF8.GetBytes(data);
+                    stream.Write(receiveData, 0, receiveData.Length);
+                }
+
+                stream.Close();
+                client.Close();
+            }
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
+        finally
+        {
+            server.Stop();
+        }
+        #endregion
+
+    }
+}
+public class PortInfo
+{
+    public int PortNumber { get; set; }
+    public string Local { get; set; }
+    public string Remote { get; set; }
+    public string State { get; set; }
+
+    public PortInfo(int portNumber, string local, string remote, string state)
+    {
+        PortNumber = portNumber;
+        Local = local;
+        Remote = remote;
+        State = state;
+    }
+}
